27eabef1c18696ea174a3e3c93e3bb5786aa40a3
likhonabolani7-dotcom <likhonabolani7@gmail.com>
2026-05-12 10:14:39 -0700

    Add files via upload

---
+++ week6_ex01_variables.js
@@ -0,0 +1,7 @@
+const firstName = "Likhona";``
+const lastName = "Bolani";
+const fullName = firstName + " " + lastName;
+let age = 25;
+console.log("Hello, my name is " + fullName + " and I am " + age + " years old.")
+age = 26;
+console.log("Next year, I will be " + age + " years old.")
