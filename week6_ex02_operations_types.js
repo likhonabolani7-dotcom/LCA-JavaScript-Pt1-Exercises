fd59a78dd19bfaf23db754773873d67127cfe73e
likhonabolani7-dotcom <likhonabolani7@gmail.com>
2026-05-13 12:39:20 -0700

    Add files via upload

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,11 @@
+const number1 = 78;
+const number2 = 22;
+
+const number1AsString = number1.toString();
+console.log(`The string representation of number1 is: ${number1AsString}`);
+
+const sum = number1 + number2;
+console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
+
+const product = number1 * number2;
+console.log(`The product of ${number1} and ${number2} is: ${product}`);
