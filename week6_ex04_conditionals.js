eef2075cddf1ed7e73fd52cab111897603fa9020
likhonabolani7-dotcom <likhonabolani7@gmail.com>
2026-05-15 15:01:45 -0700

    Add files via upload

---
+++ week6_ex04_conditionals.js
@@ -0,0 +1,11 @@
+const score = 100;
+
+const passingGrade = 60;
+
+if (score === 100) {
+    console.log("Perfect score! Passed.");
+} else if (score >= passingGrade) {
+    console.log("Passed.");
+} else {
+    console.log("Failed.");
+}
