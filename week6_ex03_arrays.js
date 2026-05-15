756091720a73180725ef254cfadc7456aba8dea0
likhonabolani7-dotcom <likhonabolani7@gmail.com>
2026-05-15 15:00:08 -0700

    Add files via upload

---
+++ week6_ex03_arrays.js
@@ -0,0 +1,17 @@
+const colors = [];
+
+colors.push("red", "blue", "green");
+
+const numbers = [10, 20, 30, 40, 50];
+
+colors.pop();
+
+colors.unshift("purple");
+
+console.log("Colors array:", colors);
+
+console.log("Length of colors array:", colors.length);
+
+console.log("Numbers array:", numbers);
+
+console.log("Length of numbers array:", numbers.length);
